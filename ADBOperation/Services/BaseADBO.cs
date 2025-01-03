61624b547937b57b65391eaaefd33eb564730b97
cirno Hakurei <knight_cirno@outlook.com>
2025-01-03 21:35:25 +0800

    新增了关于ADB操作的类库，实际逻辑和对应UI待实现。

---
+++ ADBOperation/Services/BaseADBO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADBOperation.Services
+{
+    abstract class BaseADBO
+    {
+        private BaseADBFile _adbFile;
+
+        protected ProcessStartInfo _adbPSInfo;
+
+        protected ProcessStartInfo _fBootPSInfo;
+
+        protected Process _process = new Process();
+
+        public abstract string RunCommand(string args,bool t);
+
+        public BaseADBO(BaseADBFile adbFile)
+        {
+            _adbFile = adbFile;
+           _adbPSInfo = new ProcessStartInfo
+           {
+               FileName = adbFile._AdbFilePath,
+               RedirectStandardOutput = true,
+               RedirectStandardError = true,
+               UseShellExecute = false,
+               CreateNoWindow = true
+           };
+            _fBootPSInfo = new ProcessStartInfo
+            {
+                FileName = adbFile._AdbFilePath,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+        }
+    }
+}
---
+++ ADBOperation/Services/NetADBO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADBOperation.Services
+{
+        // This Class Not implement
+    internal class NetADBO : BaseADBO
+    {
+        public NetADBO(BaseADBFile adbFile) : base(adbFile)
+        {
+        }
+
+        public override string RunCommand(string args, bool t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
